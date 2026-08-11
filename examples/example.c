0fb07fa337b931a5153d4fbba273cf89fe36d4f3
Noticxs <munchablesofficial@outlook.com>
2026-08-11 10:20:21 +0100

    updates

--- examples/example.c
+++ examples/example.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 
 int main() {
-  test();
   // wait_for(4); // waits for 4 seconds
   printf("Hello, World!\n");
   clear_window(); // clears terminal window and moves cursor to position 1,1
