2a13a8784624a2c8b640711f8252eed71a459c5d
Noticxs <munchablesofficial@outlook.com>
2026-08-31 21:11:03 +0100

    adding to crates.io

--- build.rs
+++ build.rs
@@ -21,13 +21,23 @@
 use std::env;
 use std::fs;
 use std::path::PathBuf;
+use std::process::Command;
 
 fn main() {
     let current_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
+    let status = Command::new("make")
+        .status()
+        .expect("Failed to execute `make`. Ensure make and GCC are installed.");
+
+    if !status.success() {
+        panic!("`make` failed to build the native miamore C library.");
+    }
+
     println!("cargo:rustc-link-search=native={}/build", current_dir);
     println!("cargo:rustc-link-lib=static=miamore");
 
+    println!("cargo:rerun-if-changed=Makefile");
     println!("cargo:rerun-if-changed=src/include/miamore.h");
     println!("cargo:rerun-if-changed=build/libmiamore.a");
 
